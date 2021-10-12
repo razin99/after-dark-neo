855a58be8e4fd14e1d80838f61504d77a504d7b0
Razin Idzuddin <razin99@gmail.com>
2021-10-12 21:37:44 +1100

    Defined user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,22 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Field(() => String)
+  @Column()
+  public email: string;
+
+  @Field(() => String)
+  @Column()
+  public username: string;
+
+  @Field(() => String)
+  @Column()
+  public password: string;
 }
