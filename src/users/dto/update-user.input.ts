4fc82059f0534173a87a7643a5fc4f37be399372
Razin Idzuddin <razin99@gmail.com>
2021-10-13 22:29:55 +1100

    Removed id, redundant field since update already takes in an id

--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,12 +1,9 @@
 import { CreateUserInput } from './create-user.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, Field, PartialType } from '@nestjs/graphql';
 import { Length } from 'class-validator';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field(() => Int)
-  id: number;
-
   @Length(5, 50)
   @Field(() => String)
   username?: string;
