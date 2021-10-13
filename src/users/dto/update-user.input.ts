224b6f377a9cd985142e6d31f775557f24232ed2
Razin Idzuddin <razin99@gmail.com>
2021-10-13 17:38:20 +1100

    Update user input dto updated

--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,8 +1,17 @@
 import { CreateUserInput } from './create-user.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { Length } from 'class-validator';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
   @Field(() => Int)
   id: number;
+
+  @Length(5, 50)
+  @Field(() => String)
+  username?: string;
+
+  @Length(10, 100)
+  @Field(() => String)
+  password?: string;
 }
