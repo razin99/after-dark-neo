1a1b38fef42cb787618d7842cd8851be9124ed26
Razin Idzuddin <razin99@gmail.com>
2021-10-17 16:22:44 +1100

    scaffold auth service from nest-cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { GraphqlModule } from './graphql/graphql.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [DatabaseModule, UsersModule, GraphqlModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule { }
