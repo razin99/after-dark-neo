a8631ed501d616bb85514bb9caff0fcfb39e8387
Razin Idzuddin <razin99@gmail.com>
2021-10-19 01:25:19 +1100

    Done with auth service. Removed login from auth service, handled by guards

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from 'src/users/dto/create-user.input';
-import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
@@ -10,8 +9,7 @@ export class AuthService {
   async validateWithUsername(username: string, password: string) {
     const user = await this.usersService.findOneByUsername(username);
     if (user && user.password === password) {
-      const { password, ...result } = user;
-      return result;
+      return user;
     }
     return null;
   }
@@ -19,19 +17,9 @@ export class AuthService {
   async validateWithEmail(email: string, password: string) {
     const user = await this.usersService.findOneByEmail(email);
     if (user && user.password === password) {
-      const { password, ...result } = user;
-      return result;
+      return user;
     }
     return null;
-  }
-
-  /**
-   * Login function to be called that returns a valid session token
-   * @param user to create session for
-   * @returns valid session token
-   */
-  login(user: Omit<User, 'password'>) {
-    return Buffer.from(JSON.stringify(user)).toString('base64');
   }
 
   /**
@@ -42,6 +30,6 @@ export class AuthService {
    */
   async register(createUserInput: CreateUserInput) {
     const user = await this.usersService.create(createUserInput);
-    return this.login(user);
+    return user;
   }
 }
