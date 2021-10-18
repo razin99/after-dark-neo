fa095de943760fd24de4692b23c5884af06cbaab
Razin Idzuddin <razin99@gmail.com>
2021-10-18 21:53:05 +1100

    Use classValidatorMessage to generate proper exception message

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidateInputPipe } from './pipes/validate.pipe';
+import { classValidatorMessage } from './pipes/validate.factory';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(`Serving app at: localhost:3000`);
-  app.useGlobalPipes(new ValidateInputPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({ exceptionFactory: classValidatorMessage }),
+  );
   await app.listen(3000);
 }
 bootstrap();
