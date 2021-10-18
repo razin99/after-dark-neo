2be848cf03ea21a5c382411ee97e2a09ca94fc48
Razin Idzuddin <razin99@gmail.com>
2021-10-18 18:46:55 +1100

    Add auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  login(@Req() req: any) {
+    // TODO: fix typing, avoid 'any'
+    return this.authService.login(req.user);
+  }
+}
