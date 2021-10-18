2aa67e675c8e6c1d5e0c5c74e54b082f75b3f708
Razin Idzuddin <razin99@gmail.com>
2021-10-18 22:14:41 +1100

    created guard to check if email is already used

---
+++ src/guards/user-exist.guard.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class UserExistGuard implements CanActivate {
+  constructor(private usersService: UsersService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+    return this.checkEmail(req.body.email);
+  }
+
+  /**
+   * @param email to check
+   * @returns true if found user with given email, false otherwise
+   */
+  async checkEmail(email: string) {
+    const user = await this.usersService.findOneByEmail(email);
+    if (user) throw new ForbiddenException('Email already in use');
+    return true;
+  }
+}
