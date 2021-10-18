1bf36f16e942167a7c9114608f8cc03570a37711
Razin Idzuddin <razin99@gmail.com>
2021-10-19 01:11:53 +1100

    Created serializer for session cookie

---
+++ src/auth/session.serializer.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportSerializer } from '@nestjs/passport';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
+
+/**
+ * Serializer for sessions, in cookie contains only User.id
+ * Deserializing returns the full User object
+ */
+@Injectable()
+export class SessionSerializer extends PassportSerializer {
+  constructor(private usersService: UsersService) {
+    super();
+  }
+
+  serializeUser(user: User, done: Function) {
+    done(null, user.id);
+  }
+
+  async deserializeUser(userId: string, done: Function) {
+    const user = await this.usersService.findOneById(Number(userId));
+    done(null, user);
+  }
+}
