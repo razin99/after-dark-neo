0cb1b3ec19a998b62ee79ac96dd80dc15a10e3ec
Razin Idzuddin <razin99@gmail.com>
2021-10-19 21:06:32 +1100

    Modified AuthenticatedGuard to be compatible with graphql

--- src/guards/authenticated.guard.ts
+++ src/guards/authenticated.guard.ts
@@ -1,5 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
+import { Request } from 'express';
 
 /**
  * Guard that checks if the user has session cookie attached by calling
@@ -7,12 +8,15 @@ import { Observable } from 'rxjs';
  */
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
-    // check if user is authenticated
-    // passport attaches this to the request object
-    return request.isAuthenticated();
+  canActivate(context: ExecutionContext): boolean {
+    if (context.getType() === 'http')
+      // passport attaches 'isAuthenticated' to the request object
+      return context.switchToHttp().getRequest<Request>().isAuthenticated();
+    else if (context.getType<GqlContextType>() === 'graphql') {
+      const ctx = GqlExecutionContext.create(context);
+      // if its serialializable to a User, then its authenticated
+      return !!ctx.getContext().req.user;
+    }
+    return false;
   }
 }
