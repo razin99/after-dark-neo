e8a85e2bfebf7a0bff17f9f2a84cf210ca5e2c12
Razin Idzuddin <razin99@gmail.com>
2021-10-19 21:07:02 +1100

    Use authenticated guard to make sure only authenticated user can use graphql User resolver

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -2,8 +2,11 @@ import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { UpdateUserInput } from './dto/update-user.input';
+import { UseGuards } from '@nestjs/common';
+import { AuthenticatedGuard } from 'src/guards/authenticated.guard';
 
 @Resolver(() => User)
+@UseGuards(AuthenticatedGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
