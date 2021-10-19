c6a3475970896563ece2e296d788195612efdc74
Razin Idzuddin <razin99@gmail.com>
2021-10-19 23:57:24 +1100

    Change 'data' to underscore, since variable is not in use

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,12 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 
-export const GetUser = createParamDecorator(
-  (data: any, ctx: ExecutionContext) => {
-    if (ctx.getType() === 'http') {
-      return ctx.switchToHttp().getRequest().user;
-    } else if (ctx.getType<GqlContextType>() === 'graphql') {
-      return GqlExecutionContext.create(ctx).getContext().req.user;
-    }
-  },
-);
+export const GetUser = createParamDecorator((_: any, ctx: ExecutionContext) => {
+  if (ctx.getType() === 'http') {
+    return ctx.switchToHttp().getRequest().user;
+  } else if (ctx.getType<GqlContextType>() === 'graphql') {
+    return GqlExecutionContext.create(ctx).getContext().req.user;
+  }
+});
