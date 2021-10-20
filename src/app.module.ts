6ba75091dfa277346d7dee17ad0a1d9f54792069
Razin Idzuddin <razin99@gmail.com>
2021-10-20 21:31:55 +1100

    Added joi validation for postgres and node environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,31 @@ import { GraphqlModule } from './graphql/graphql.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, GraphqlModule, AuthModule, PostsModule],
+  imports: [
+    DatabaseModule,
+    UsersModule,
+    GraphqlModule,
+    AuthModule,
+    PostsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_HOST: Joi.string().required(),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+      }),
+      validationOptions: {
+        abortEarly: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, AuthService],
 })
