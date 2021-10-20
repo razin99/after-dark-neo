354b1815ab71ffbe6c16c2d2fcf5c1ca323c6c2e
Razin Idzuddin <razin99@gmail.com>
2021-10-20 21:54:42 +1100

    Load redis host and port, and session secret from env

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 import { classValidatorMessage } from './pipes/validate.factory';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import { ConfigService } from '@nestjs/config';
+import { createClient } from 'redis';
+import * as createRedisStore from 'connect-redis';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,9 +18,18 @@ async function bootstrap() {
     }),
   );
 
+  const conf = app.get(ConfigService);
+
+  const RedisStore = createRedisStore(session);
+  const redisClient = createClient({
+    host: conf.get('REDIS_HOST'),
+    port: conf.get('REDIS_PORT'),
+  });
+
   app.use(
     session({
-      secret: 'reeeeeeeeeeeeeeeeeeeeee',
+      store: new RedisStore({ client: redisClient }),
+      secret: conf.get('SESSION_SECRET'),
       resave: false,
       saveUninitialized: false,
     }),
