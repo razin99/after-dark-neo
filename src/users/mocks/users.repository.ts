53aca8d86ffe89edf2dbbc48b7ebf672b67d56d2
Razin Idzuddin <razin99@gmail.com>
2021-10-21 23:55:08 +1100

    Changed mock repo to use User instead of a dto

--- src/users/mocks/users.repository.ts
+++ src/users/mocks/users.repository.ts
@@ -1,22 +1,25 @@
-import { CreateUserInput } from '../dto/create-user.input';
 import { User } from '../entities/user.entity';
 
-export const allUsers: CreateUserInput[] = [
+export const allUsers: User[] = [
   {
+    id: 1,
     username: 'bobby',
     email: 'yeetboi@gmail.com',
     password: 'tekashi6Nine',
+    posts: [],
   },
   {
+    id: 2,
     username: 'greg',
     email: 'greg69@hotmail.com',
     password: 'rockyou',
+    posts: [],
   },
 ];
 
 export const userRepositoryFactory = () => ({
   create: jest.fn((user: User) => ({ ...user, id: 1 })),
-  findOne: jest.fn((id: number) => allUsers[id]),
+  findOne: jest.fn((id: number) => allUsers.find((user) => user.id === id)),
   find: jest.fn(() => allUsers),
   delete: jest.fn((id: number) => ({
     affected: allUsers.length > id && id >= 0 ? 1 : 0,
