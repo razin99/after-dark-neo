337e54512a9e44ccae9f9d1d316d68916a1427e5
Razin Idzuddin <razin99@gmail.com>
2021-10-22 04:52:43 +1100

    Finished auth service tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,4 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import {
+  allUsers,
+  userRepositoryFactory,
+} from 'src/users/mocks/users.repository';
+import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -6,7 +13,14 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useFactory: userRepositoryFactory,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +28,33 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it(`should validate username with correct password`, async () => {
+    const user = allUsers[0];
+    expect(
+      await service.validateWithUsername(user.username, user.password),
+    ).toEqual(user);
+  });
+
+  it(`should fail to validate username with incorrect password`, async () => {
+    const user = allUsers[0];
+    expect(
+      await service.validateWithUsername(user.username, `--${user.password}--`),
+    ).toEqual(null);
+  });
+
+  it(`should validate email with correct password`, async () => {
+    const user = allUsers[1];
+    expect(await service.validateWithEmail(user.email, user.password)).toEqual(
+      user,
+    );
+  });
+
+  it(`should fail to validate email with incorrect password`, async () => {
+    const user = allUsers[1];
+    expect(
+      await service.validateWithEmail(user.email, `--${user.password}--`),
+    ).toEqual(null);
   });
 });
