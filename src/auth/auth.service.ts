b70e179bcaafc147a1456c3456f11879e04022a8
Razin Idzuddin <razin99@gmail.com>
2021-10-22 06:27:43 +1100

    Use NODE_ENV to change password storage and check behaviour
    On prod, uses bcrypt. On dev, just plaintext

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { CreateUserInput } from 'src/users/dto/create-user.input';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private conf: ConfigService,
+  ) {}
 
   /**
    * Validate user with username and password
@@ -16,7 +21,9 @@ export class AuthService {
     password: string,
   ): Promise<User | null> {
     const user = await this.usersService.findOneByUsername(username);
-    if (user && user.password === password) {
+    if (!user) return null;
+    const matchSuccess = await this.matchPassword(password, user.password);
+    if (matchSuccess) {
       return user;
     }
     return null;
@@ -31,7 +38,9 @@ export class AuthService {
     password: string,
   ): Promise<User | null> {
     const user = await this.usersService.findOneByEmail(email);
-    if (user && user.password === password) {
+    if (!user) return null;
+    const matchSuccess = await this.matchPassword(password, user.password);
+    if (matchSuccess) {
       return user;
     }
     return null;
@@ -44,7 +53,20 @@ export class AuthService {
    * @returns valid session token
    */
   async register(createUserInput: CreateUserInput) {
+    if (this.conf.get<string>('NODE_ENV') === 'production')
+      createUserInput.password = await bcrypt.hash(createUserInput.password, 5);
     const user = await this.usersService.create(createUserInput);
     return user;
+  }
+
+  private async matchPassword(
+    userPass: string,
+    dbPass: string,
+  ): Promise<boolean> {
+    if (this.conf.get<string>('NODE_ENV') === 'development') {
+      return userPass === dbPass;
+    } else {
+      return bcrypt.compare(userPass, dbPass);
+    }
   }
 }
