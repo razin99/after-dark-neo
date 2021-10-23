01de4a37e768c47efd3dedf87e3d8857394604cd
Razin Idzuddin <razin99@gmail.com>
2021-10-24 09:20:42 +1100

    Initialize firebase connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import * as admin from 'firebase-admin';
+import * as fireorm from 'fireorm';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
+  imports: [ConfigModule],
+  providers: [
+    {
+      provide: 'FIRESTORE',
       inject: [ConfigService],
-      useFactory: (conf: ConfigService) => ({
-        type: 'postgres',
-        host: conf.get('POSTGRES_HOST'),
-        port: conf.get('POSTGRES_PORT'),
-        username: conf.get('POSTGRES_USER'),
-        password: conf.get('POSTGRES_PASSWORD'),
-        database: 'afterdark',
-        entities: ['dist/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-    }),
+      useFactory: async (conf: ConfigService) => {
+        admin.initializeApp({
+          credential: admin.credential.cert(conf.get<string>('SERVICE_KEY')),
+          projectId: conf.get<string>('PROJECT_ID'),
+        });
+        fireorm.initialize(admin.firestore());
+      },
+    },
   ],
+  exports: ['FIRESTORE'],
 })
 export class DatabaseModule {}
