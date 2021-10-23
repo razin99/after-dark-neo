4445d4e6121a73c37aad4b14d2395f5fe3ab7dc4
Razin Idzuddin <razin99@gmail.com>
2021-10-24 09:16:23 +1100

    Change post entity to be firebase compatible

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,34 +1,17 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { User } from 'src/users/entities/user.entity';
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { Timestamp } from '@google-cloud/firestore';
+import { ObjectType, Field } from '@nestjs/graphql';
 
 @ObjectType()
-@Entity()
 export class Post {
-  @Field(() => Int)
-  @PrimaryGeneratedColumn()
-  id: number;
+  @Field(() => String)
+  id: string;
 
   @Field(() => String)
-  @Column()
   body: string;
 
   @Field(() => Date)
-  @CreateDateColumn()
-  created_at: Date;
+  created_at: Timestamp;
 
   @Field(() => Date)
-  @UpdateDateColumn()
-  updated_at: Date;
-
-  @Field(() => User)
-  @ManyToOne(() => User, (user) => user.posts, { onDelete: 'CASCADE' })
-  author: User;
+  updated_at: Timestamp;
 }
