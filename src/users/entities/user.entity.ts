ef59564d39415f6bdb773fa68eeba5b240e5a528
Razin Idzuddin <razin99@gmail.com>
2021-10-24 09:07:01 +1100

    Edit user entity to use fireorm, posts field is subcollection

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,26 +1,28 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Timestamp } from '@google-cloud/firestore';
+import { ObjectType, Field } from '@nestjs/graphql';
 import { Exclude } from 'class-transformer';
+import { Collection, ISubCollection, SubCollection } from 'fireorm';
 import { Post } from 'src/posts/entities/post.entity';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
-@Entity()
+@Collection()
 export class User {
-  @Field(() => Int)
-  @PrimaryGeneratedColumn()
-  public id: number;
+  @Field(() => String)
+  public id: string;
 
-  @Column({ unique: true })
   public email: string;
 
   @Field(() => String)
-  @Column()
   public username: string;
 
-  @Exclude()
-  @Column()
+  @Exclude({ toPlainOnly: true })
   public password: string;
 
-  @OneToMany(() => Post, (post) => post.author, { onDelete: 'CASCADE' })
-  posts: Post[];
+  @SubCollection(Post)
+  @Field(() => [Post], { nullable: true })
+  @Exclude()
+  posts?: ISubCollection<Post>;
+
+  @Field(() => Date)
+  created_at: Timestamp;
 }
