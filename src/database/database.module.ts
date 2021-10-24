818a7b43fe9862dd360232d00d5959947c26d45d
Razin Idzuddin <razin99@gmail.com>
2021-10-25 03:00:14 +1100

    Injecting firestore gives back 'firebase' db

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,30 +3,32 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as admin from 'firebase-admin';
 import * as fireorm from 'fireorm';
 
+export const FIRESTORE = 'firestore';
+
 @Module({
   imports: [ConfigModule],
   providers: [
     {
-      provide: 'FIRESTORE',
+      provide: FIRESTORE,
       inject: [ConfigService],
       useFactory: async (conf: ConfigService) => {
+        let db = admin.firestore();
         if (conf.get<string>('NODE_ENV') === 'production') {
           admin.initializeApp({
             credential: admin.credential.cert(conf.get<string>('SERVICE_KEY')),
             projectId: conf.get<string>('PROJECT_ID'),
           });
-          fireorm.initialize(admin.firestore());
         } else {
           admin.initializeApp();
-          let db = admin.firestore();
           db.settings({
             host: `${conf.get<string>('FIRESTORE_HOST')}:${conf.get<number>(
               'FIRESTORE_PORT',
             )}`,
             ssl: false,
           });
-          fireorm.initialize(db);
         }
+        fireorm.initialize(db);
+        return db;
       },
     },
   ],
