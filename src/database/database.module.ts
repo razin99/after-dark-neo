c3a89e40348b7462d3afe3a32be5408b56feb476
Razin Idzuddin <razin99@gmail.com>
2021-10-25 04:49:12 +1100

    Cleanup firebase initialisation logic
    Simpler ternary statements instead of repeated if else logic

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,37 +1,36 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as admin from 'firebase-admin';
 import * as fireorm from 'fireorm';
+import { ApiConfigModule } from 'src/api-config/api-config.module';
+import { ApiConfigService } from 'src/api-config/api-config.service';
 
 export const FIRESTORE = 'firestore';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ApiConfigModule],
   providers: [
     {
       provide: FIRESTORE,
-      inject: [ConfigService],
-      useFactory: async (conf: ConfigService) => {
+      inject: [ApiConfigService],
+      useFactory: async (conf: ApiConfigService) => {
+        conf.NODE_ENV === 'production'
+          ? admin.initializeApp({
+              credential: admin.credential.cert(conf.SERVICE_KEY),
+              projectId: conf.PROJECT_ID,
+            })
+          : admin.initializeApp();
+
         let db = admin.firestore();
-        if (conf.get<string>('NODE_ENV') === 'production') {
-          admin.initializeApp({
-            credential: admin.credential.cert(conf.get<string>('SERVICE_KEY')),
-            projectId: conf.get<string>('PROJECT_ID'),
-          });
-        } else {
-          admin.initializeApp();
+        conf.NODE_ENV === 'development' &&
           db.settings({
-            host: `${conf.get<string>('FIRESTORE_HOST')}:${conf.get<number>(
-              'FIRESTORE_PORT',
-            )}`,
+            host: `${conf.FIRESTORE_HOST}:${conf.FIRESTORE_PORT}`,
             ssl: false,
           });
-        }
         fireorm.initialize(db);
         return db;
       },
     },
   ],
-  exports: ['FIRESTORE'],
+  exports: [FIRESTORE],
 })
 export class DatabaseModule {}
